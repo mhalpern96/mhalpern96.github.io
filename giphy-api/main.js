0ef0c0a92453442e3c5b9196aa8f4959716c7c66
Michael Halpern <mhalpern@sbcglobal.net>
2019-01-04 21:20:37 -0500

    regex 1

--- giphy-api/main.js
+++ giphy-api/main.js
@@ -34,7 +34,7 @@ console.log(name);
 var xhr = $.get("https://cors.io/?https://api.giphy.com/v1/gifs/search?q="+ name + "&api_key=WHqh7w7DEBAvxr73OzKUIhesiCXQiZIR&limit=10");
 xhr.done(function(data){
 //render images
-	data = unescape(data);
+	data = data.replace(/[^a-z0-9\s]/gi, '');
     for (i=0; i < data.data.length; i++){
     var gifDiv = $("<div class='item'>");
    gifDiv.addClass("btn")
