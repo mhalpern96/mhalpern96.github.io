b6efc189d337bb4a76c5275a19cf13f1a81ca6b2
Michael Halpern <mhalpern@sbcglobal.net>
2020-02-12 13:12:32 -0500

    commit

---
+++ IT1050/week 5/week5sprint 1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace week5sprint_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+            int x = 0;
+            while(i< 10)
+            {
+                Console.WriteLine("input a number");
+                
+                x = x + int.Parse(Console.ReadLine());
+                i++;
+            }
+            Console.WriteLine("the sum is" + x);
+            Console.ReadKey();
+        }
+    }
+}
